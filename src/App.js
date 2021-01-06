33fe33c72bf51bcac34ce8432006861273d687df
agosh-8372 <agoshchauhan378@gmail.com>
2021-01-06 18:41:10 +0530

    Displayed Phone Directory

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 import React, {Component} from 'react';
 
 class App extends Component {
   render(){
   return (
     <div>
+    Phone Directory
     </div>
   );
   }
